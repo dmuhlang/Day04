5254406f1e2164959c09026be47e459c2785eda7
Danisa Muhlanga <dmuhlang@c5r3s4.wethinkcode.co.za>
2020-06-24 17:01:17 +0200

    done

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmuhlang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/24 16:49:02 by dmuhlang          #+#    #+#             */
+/*   Updated: 2020/06/24 16:57:34 by dmuhlang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0 || nb > 12)
+		return (0);
+	if (nb <= 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+int main()
+{
+	int a,b;
+	a= 10;
+	b = ft_recursive_factorial(a);
+	printf("%d", b);
+	return(0);
+}
+
